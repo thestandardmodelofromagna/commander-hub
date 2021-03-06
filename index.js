ab76351f4463cbc7d963cd1f63a7f3a5dd3e4069
Daniele Tentoni <d.tentoni@wedoit.io>
2021-03-06 17:38:43 +0100

    Add a call from client to server.

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ app.get('/', function (req, res) { // At <url>/
 });
 
 /*
+ * GET: Given a name, fetch scryfall to get all cards named.
+ */
+app.get('/card/:name', function (req, res) {
+    // Call py script.
+    // Generate data.
+    let name = req.params.name;
+    let card = 'one card: ' + name;
+    console.log("Called card with name: " + name);
+    // Send data.
+    res.send(card);
+});
+
+/*
  * POST: Create a resource from server.
  */
 /*
@@ -23,3 +36,18 @@ app.get('/', function (req, res) { // At <url>/
 app.listen(8080, function() {
     console.log("I'm ready");
 });
+
+// Setup a catch-all point.
+app.get('*', (req, res) => res.status(404).send({
+    message: 'Here there\'s nothing. 404?'
+}));
+
+// Error handler.
+app.use(function (err, req, res, next) {
+    console.error("Last error handler.");
+    console.error(err.message);
+    if(!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+});
+
+module.exports = app;
