b09e46d4b6d9049000536dad91e8231ad968ba3d
Daniele Tentoni <d.tentoni@wedoit.io>
2021-03-20 16:22:50 +0100

    Serve static files.

--- index.js
+++ index.js
@@ -9,36 +9,47 @@
 
 // Require api calls library.
 const express = require('express');
-const { urlencoded, json } = require('body-parser')
+const { urlencoded, json } = require('body-parser');
+const path = require('path');
 const cardRoute = require('./routes/cardRoute');
 const userRoute = require('./routes/userRoute');
+
+/*
+ * Helmet can help protect your app from some well-known web vulnerabilities by setting HTTP headers appropriately.
+ * https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
+ */
+const helmet = require('helmet');
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
 })
-    .then(() => {
-        console.log("Database connected");
-    }, (error) => {
-        console.log("Database connection error ", error);
-    });
+  .then(() => {
+    console.log("Database connected");
+  }, (error) => {
+    console.log("Database connection error ", error);
+  });
 
 // Create base entrypoint.
 const app = express();
 app.use(json());
 app.use(urlencoded({ extended: false }));
+// app.use(helmet());
 cardRoute(app);
 userRoute(app);
 
+// Account routes will be available at /account/...
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
-    res.sendFile(`${__dirname}/public/index.html`);
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get('/login', function (req, res) {
-    res.sendFile(`${__dirname}/public/login.html`);
+  res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
 // Card endpoint moved to card route script.
@@ -46,24 +57,24 @@ app.get('/login', function (req, res) {
 const port = process.env.PORT || 8080;
 
 app.listen(port, function () {
-    console.log(`You are in ${process.env.NODE_ENV} environment.`);
-    console.log(`Server ready to accept requests on http://localhost:${port}`);
+  console.log(`You are in ${process.env.NODE_ENV} environment.`);
+  console.log(`Server ready to accept requests on http://localhost:${port}`);
 });
 
 // Setup a catch-all point.
 app.get('*', (req, res) => res.status(404).send({
-    message: 'Here there\'s nothing. 404?'
+  message: 'Here there\'s nothing. 404?'
 }));
 
 // Error handler.
 app.use(function (err, req, res, next) {
-    console.error("Last error handler.");
-    console.error(err.message);
+  console.error("Last error handler.");
+  console.error(err.message);
 
-    if (!err.statusCode)
-        err.statusCode = 500;
+  if (!err.statusCode)
+    err.statusCode = 500;
 
-    res.status(err.statusCode).send(err.message);
+  res.status(err.statusCode).send(err.message);
 });
 
 module.exports = app;
